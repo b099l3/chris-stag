dd59c7d63edf8ca8fb3b56ef5b3bcb5508d936c0
Iain Smith <b099l3@gmail.com>
2022-02-15 13:17:06 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,15 @@ export default function Home() {
         <Header title="The Plan!" />
         <p className="description">
           <ul>
-          <li><a href="https://www.salthorse.beer/drink-food#drinks">1pm: Meet at Salt Horse</a> for swift beer and/or snacks (a table is booked for 6 under Chris' name) </li>
+          <li>1pm: <a href="https://www.salthorse.beer/drink-food#drinks">Meet at Salt Horse</a>for swift beer and/or snacks (a table is booked for 6 under Chris' name) </li>
           <li>1:57pm: Catch 31 bus from the stop on South Bridge to New Dundas Park (a later bus can be taken if required)</li>
           <li>2:45pm: Arrive at New Dundas Park (£7 on the door) </li>
           <li>3pm: Kick off - Bonnyrigg Rose vs Bo'Ness Utd </li>
           <li>4:45pm: Match Ends </li>
           <li>4:53pm: Catch 31 bus back to Palmerston Place </li>
-          <li><a href="https://www.thepalmerstonedinburgh.co.uk/">6:30pm: Dinner at the Palmerston </a></li>
-          <li><a href="https://vaultcity.co.uk/pages/the-wee-vault-tasting-room-and-bottleshop">8:30pm: Drinks at The Wee Vault</a></li>
-          <li></li>
+          <li>6:30pm: <a href="https://www.thepalmerstonedinburgh.co.uk/">Dinner at the Palmerston </a></li>
+          <li>8:30pm: <a href="https://vaultcity.co.uk/pages/the-wee-vault-tasting-room-and-bottleshop">Drinks at The Wee Vault</a></li>
+          <li>10pm: Lucky Liquor</li>
           </ul>
         </p>
       </main>
