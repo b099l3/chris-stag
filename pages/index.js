cf1376a334f4e5a40415e53b5d1fce2ff45218b5
Iain Smith <b099l3@gmail.com>
2022-02-16 02:43:22 +0000

    fix for mobile

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ export default function Home() {
       <Head>
         <title>Chris' Stag Party!</title>
         <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
       <main>
         <Header />
